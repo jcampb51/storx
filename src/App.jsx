03a6c0cb3873a676bede34f24221c50c040b56b8
James Campbell <jamesrcampbell.obb@gmail.com>
2024-07-24 10:15:11 -0500

    finished making the edit component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
-import { StorxTicker } from "./components/storx/StorxTicker"
+import { useEffect, useState } from "react";
+import { EditStorx } from "./components/storx/EditStorx";
+import { StorxTicker } from "./components/storx/StorxTicker";
+import { getAllStorx } from "./services/storxService"; // Adjust the import path as necessary
+import { getAllTypes } from "./services/typeService";
 
 export const App = () => {
+  const [allStorx, setAllStorx] = useState([]);
+  const [allTypes, setAllTypes] = useState([])
+
+  const getAndSetAllStorx = () => {
+    getAllStorx().then((storxArray) => {
+      setAllStorx(storxArray);
+    });
+  };
+
+  const getAndSetAllTypes = () => {
+    getAllTypes().then((typesArray) => {
+      setAllTypes(typesArray)
+    })
+  }
+
+  useEffect(() => {
+    getAndSetAllStorx();
+  }, []);
+
+  useEffect(() => {
+    getAndSetAllTypes()
+  }, [])
+
   return (
-    <StorxTicker />
-  )
-}
+    <div>
+      <EditStorx allStorx={allStorx} allTypes={allTypes} />
+      <StorxTicker allStorx={allStorx} />
+    </div>
+  );
+};
+
