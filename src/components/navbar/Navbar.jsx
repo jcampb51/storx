86cadb61273d677c7e2b36926de4d081e56082e8
James Campbell <jamesrcampbell.obb@gmail.com>
2024-07-25 10:35:27 -0500

    added code for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,1 +1,40 @@
-//create and export a render function and define the links for the Navbar. Make sure useNavigate hook is correctly imported and define it as a variable to use it properly
+//create and export a render function and define the links for the Navbar. 
+//Make sure useNavigate hook is correctly imported and define it as a variable to use it properly
+
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const Navebar = () => {
+    const navigate = useNavigate
+    return (
+    <ul className="navbar">
+        <li className="navbar-item">
+            <Link to="/ticker">Storx Ticker</Link>
+        </li>
+        <li className="navbar-item">
+            <Link to="/user_profile">My Profile</Link>
+        </li>
+        <li className="navbar-item">
+            <Link to="/users">Users</Link>
+        </li>
+        <li className="navbar-item">
+            <Link to="/create">Create Storx</Link>
+        </li>
+    
+    {localStorage.getItem("honey_user") ? (
+        <li className="navbar-item navbar-logout">
+          <Link
+            className="navbar-link"
+            to=""
+            onClick={() => {
+              localStorage.removeItem("storx_user")
+              navigate("/", { replace: true })
+            }}
+          >
+            Logout
+          </Link>
+        </li>
+      ) : (
+        ""
+      )}</ul>
+)}
